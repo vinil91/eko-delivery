d3804def3239982d95ffe1957cd5cd661e706620
vinil91 <dnvictr@gmail.com>
2020-09-10 14:24:40 +0300

    Change form class to func

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,69 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import { block } from 'bem-cn';
 
 const f = block('form');
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+function Form({ description, caseAB, caseForm, placeholder, onEnter }) {
+  const [value, setValue] = useState('');
 
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const { value } = this.state;
-    const { onEnter } = this.props;
     if (value) {
       onEnter(value);
-      this.setState({ value: '' });
+      setValue('');
     }
   }
 
-  handleChange(event) {
-    const { caseForm } = this.props;
-    let value = event.target.value.toUpperCase();
+  const handleChange = (event) => {
+    let curValue = event.target.value.toUpperCase();
     if (caseForm) {
-      value = value.replace(/[^A-Za-z]/ig, '');
+      curValue = curValue.replace(/[^A-Za-z]/ig, '');
     } else {
-      value = value.replace(/[^A-Za-z0-9,]/ig, '');
+      curValue = curValue.replace(/[^A-Za-z0-9,]/ig, '');
     }
-    this.setState({ value });
+    setValue(curValue);
   }
 
-  render() {
-    const { description, caseAB, placeholder } = this.props;
-    const { value } = this.state;
-    return (
-      <div>
-        <h4 className="text-with-indent">{description}</h4>
-        <form
-          className={f()}
-          onSubmit={this.handleSubmit}
+  return (
+    <div>
+      <h4 className="text-with-indent">{description}</h4>
+      <form
+        className={f()}
+        onSubmit={handleSubmit}
+      >
+        <input
+          className={f('input')}
+          type="text"
+          maxLength={caseAB ? 2 : undefined}
+          placeholder={`Type it here, ${placeholder}`}
+          value={value}
+          onChange={handleChange}
+        />
+        <button
+          className={f('button')}
+          type="submit"
         >
-          <input
-            className={f('input')}
-            type="text"
-            maxLength={caseAB ? 2 : undefined}
-            placeholder={`Type it here, ${placeholder}`}
-            value={value}
-            onChange={this.handleChange}
-          />
-          <button
-            className={f('button')}
-            type="submit"
-          >
-            ENTER
-          </button>
-        </form>
-      </div>
-    );
-  }
+          ENTER
+        </button>
+      </form>
+    </div>
+  );
 }
 
 Form.propTypes = {
